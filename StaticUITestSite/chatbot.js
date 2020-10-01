84d45cc1aae1436ce5dbe8306c04ada8625435f5
christian.hart <christian.hart@tacticedge.com>
2020-10-01 09:12:56 -0400

    test commenting out stuff we don't need

--- StaticUITestSite/chatbot.js
+++ StaticUITestSite/chatbot.js
@@ -28,13 +28,13 @@ function initalizeConnection()  {
     console.log("this is the first name:" + customerName);
     console.log("this is the username: " + username);
     connect.ChatInterface.initiateChat({
-        name: customerName,
-        username: username,
+        //name: customerName,
+        //username: username,
         region: "us-east-1",
         apiGatewayEndpoint: "https://fa6jstm6z1.execute-api.us-east-1.amazonaws.com/Prod",
-        contactAttributes: JSON.stringify({
-            "customerName": customerName
-        }),
+        //contactAttributes: JSON.stringify({
+        //    "customerName": customerName
+        //}),
         contactFlowId: "bd8bf79c-68f4-4d05-869e-056f958eb39f",
         instanceId: "dd3cb455-ab3e-4827-a079-1b86968ca2fb"
     }, successHandler, failureHandler)
