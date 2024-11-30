2bf7d4979a8afb5148f05065ada1b955610b337d
Haoming Li <haominli@amazon.com>
2024-11-29 22:09:26 -0800

    Add example code to integrate push notification

---
+++ mobileChatExamples/iOS-WKWebView-sample/PushNotificationExtension/NotificationService.swift
@@ -0,0 +1,39 @@
+//
+//  NotificationService.swift
+//  PushNotificationExtension
+//  WkWebView Demo
+//
+//  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//  SPDX-License-Identifier: MIT-0
+//
+
+import UserNotifications
+
+class NotificationService: UNNotificationServiceExtension {
+
+    var contentHandler: ((UNNotificationContent) -> Void)?
+    var bestAttemptContent: UNMutableNotificationContent?
+
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        self.contentHandler = contentHandler
+        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+        if let bestAttemptContent = bestAttemptContent {
+            // Modify the bestAttemptContent object to change notification content
+            bestAttemptContent.title = "New message from chat"
+            bestAttemptContent.body = "Tap to view the message"
+            print("background received message: \(bestAttemptContent.userInfo as NSDictionary as! [String: AnyObject])")
+            bestAttemptContent.title = "\(bestAttemptContent.title)"
+            bestAttemptContent.body = "\(bestAttemptContent.body)"
+            contentHandler(bestAttemptContent)
+        }
+    }
+    
+    override func serviceExtensionTimeWillExpire() {
+        // Called just before the extension will be terminated by the system.
+        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+            contentHandler(bestAttemptContent)
+        }
+    }
+
+}
