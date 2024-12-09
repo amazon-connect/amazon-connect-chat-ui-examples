85453ee25f651f67f3f4aaaf6a54a648095b5307
haomingli2020 <74380769+haomingli2020@users.noreply.github.com>
2024-12-09 05:19:01 -0800

    Add example code to integrate push notification (#251)

--- mobileChatExamples/android-webview-sample/app/src/main/java/com/amazonaws/android_webview_sample/MainActivity.kt
+++ mobileChatExamples/android-webview-sample/app/src/main/java/com/amazonaws/android_webview_sample/MainActivity.kt
@@ -7,21 +7,114 @@
  */
 package com.amazonaws.android_webview_sample
 
+import android.content.ContentValues.TAG
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.navigation.Navigation
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.amazonaws.android_webview_sample.databinding.ActivityMainBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
     private val appBarConfiguration: AppBarConfiguration? = null
+    companion object {
+        var pushEnabled = true;
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(
             layoutInflater
         )
         setContentView(binding.root)
+
+        askNotificationPermission()
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            Log.d(TAG, "Device token: ${token}")
+            Toast.makeText(baseContext, token, Toast.LENGTH_SHORT).show()
+        })
+    }
+
+    // Declare the launcher at the top of your Activity/Fragment:
+    private val requestPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission(),
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // FCM SDK (and your app) can post notifications.
+        } else {
+            // TODO: Inform user that that your app will not show notifications.
+        }
+    }
+
+    private fun askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) ==
+                    PackageManager.PERMISSION_GRANTED
+            ) {
+                // FCM SDK (and your app) can post notifications.
+            } else if (shouldShowRequestPermissionRationale(android.Manifest.permission.POST_NOTIFICATIONS)) {
+                // TODO: display an educational UI explaining to the user the features that will be enabled
+                //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
+                //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
+                //       If the user selects "No thanks," allow the user to continue without notifications.
+            } else {
+                // Directly ask for the permission
+                requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG,"Lifecycle event received: onStop");
+        // resume notification
+        pushEnabled = true;
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG,"Lifecycle event received: onPause");
+        // resume notification
+        pushEnabled = true;
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG,"Lifecycle event received: onDestroy");
+        // resume notification
+        pushEnabled = true;
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG,"Lifecycle event received: onRestart");
+        // stop notification
+        pushEnabled = false;
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG,"Lifecycle event received: onResume");
+        // stop notification
+        pushEnabled = false;
     }
 
     override fun onSupportNavigateUp(): Boolean {
